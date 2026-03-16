1214b23009391733e79543070db144ddde7e53dc
John Myles <johnmyles1604@gmail.com>
2026-03-16 10:19:48 +0000

    Enhance products page with styling and structure
    Added CSS styling for card images and ensured proper HTML structure.

--- pages/products.php
+++ pages/products.php
@@ -11,16 +11,20 @@ require 'connect_db.php';
   <title>MK Time</title>
 
   
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
-        rel="stylesheet" 
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
-        crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="style.css" rel="stylesheet" type="text/css">
-<link href='https://fonts.googleapis.com/css?family=Alegreya SC' rel='stylesheet'>
- 
-  </head>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+
+  <link href='https://fonts.googleapis.com/css?family=Alegreya SC' rel='stylesheet'>
+
+<style>
+
+.card-img-top {
+  height: 200px; 
+  object-fit: cover;
+}
+</style>
+</head>
 
 
   <body>
